043eb07d079057896729366e0e13090fbce41765
bm21798 <bm21798@essex.ac.uk>
2023-11-10 15:10:20 +0000

    completed ex3

--- part2_NLTK_studentID.py
+++ part2_NLTK_studentID.py
@@ -1,2 +1,16 @@
+#requires user to call nltk.download() in the shell first and download book
+
 import nltk
-from nltk.book import book7
+from nltk.book import text7
+from collections import Counter
+
+wsj =[]
+wsj = text7.tokens
+
+wordCounts = Counter(wsj)
+
+mostCommon = wordCounts.most_common(50)
+i = 1;
+for string,count in mostCommon:
+    print(f"{i}){string}")
+    i+=1
