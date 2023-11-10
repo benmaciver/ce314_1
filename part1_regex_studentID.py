1eea8e7581d7dedea68bb2155f13884c5b3eb6be
bm21798 <bm21798@essex.ac.uk>
2023-11-10 14:53:26 +0000

    ex2

--- part1_regex_studentID.py
+++ part1_regex_studentID.py
@@ -1,14 +1,24 @@
 import re
+#ex1
 pattern = r'(?:\d+(?:,|\.)?\d+(?:bn|m)?(?:\seuros|\seuro|p))|(?:(?:£|\$)\d+(?:,|\.)?\d+(?:bn|m)?)'
 
 a = open("bbctxt.txt","r")
 bbcText = a.read()
-b = open("test.txt","r")
-testText = b.read()
 
 
-matches1 = re.findall(pattern, bbcText)
-print(matches1)
-matches2 = re.findall(pattern, testText)
-print(matches2)
 
+matches = re.findall(pattern, bbcText)
+print(matches)
+
+
+#ex2
+pattern = r'(?:(?:(?:(?:(?:\+1)|2|3)-))?(?:\(\d{3}\))?(?:\s|\-))?(?:\d(?:\.|-|\s)?)+'
+
+c = open("phone_numbers.txt","r")
+phonebook = c.read()
+
+matches = re.findall(pattern, phonebook)
+for match in matches:
+    print(match)
+
+    #matches every phone number correctly except 2nd and 3rd
