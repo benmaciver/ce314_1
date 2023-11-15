119c0203b04da0791d9d612b5230ee8e20bdd1ec
bm21798 <bm21798@essex.ac.uk>
2023-11-15 16:36:35 +0000

    added comments

--- part1_regex_studentID.py
+++ part1_regex_studentID.py
@@ -1,11 +1,11 @@
 import re
 #ex1
-pattern = r'(?:\d+(?:,|\.)?\d+(?:bn|m)?(?:\seuros|\seuro|p))|(?:(?:£|\$)\d+(?:,|\.)?\d+(?:bn|m)?)'
+pattern = r'(?:\d+(?:,|\.)?\d+(?:bn|m)?(?:\seuros|\seuro|p))|(?:(?:£|\$)\d+(?:,|\.)?\d+(?:bn|m)?)'#re pattern for currency
 
 a = open("bbctxt.txt","r")
-bbcText = a.read()
+bbcText = a.read()#reads the bbc article
 b = open("test.txt","r")
-testText = b.read()
+testText = b.read()#some test data from the assignment brief to test for more ways to type currency
 
 
 
@@ -13,16 +13,19 @@ matches = re.findall(pattern, bbcText)
 print(matches)
 matches = re.findall(pattern, testText)
 print(matches)
+#prints every time this re is matched in both texts
 
 
 #ex2
+#re for phone numbers
 pattern = r'(?:(?:(?:(?:(?:\+1)|2|3)-))?(?:\(\d{3}\))?(?:\s|\-))?(?:\d(?:\.|-|\s)?)+'
 
-c = open("phone_numbers.txt","r")
+c = open("phone_numbers.txt","r")#phone numbers from assignment brief
 phonebook = c.read()
 
 matches = re.findall(pattern, phonebook)
 for match in matches:
     print(match)
+    #prints each match in a list
 
     #matches every phone number correctly except 2nd and 3rd
--- part3_LM_studentID.py
+++ part3_LM_studentID.py
@@ -1,50 +1,64 @@
 import nltk
-from nltk import word_tokenize
-from nltk import tokenize
-from nltk.util import ngrams
-from nltk.corpus import reuters
 from nltk import bigrams, FreqDist
-from nltk.corpus import stopwords
+
+#helper functions
+def is_punctuation(char):
+    return char in "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+
+#question 1
+print("Question 1:")
+#Get corpus
+reuters = nltk.corpus.reuters.words()
+
+#Clean and prepare corpus
+filtered_content=[]
+for word in reuters:
+    if word[0] != word[-1]:
+        filtered_content.append(word.lower())
+    elif is_punctuation(word[0])!= True:
+        filtered_content.append(word.lower())
+
+#Create bigrams and get frequency distribution  
+bigrams = bigrams(filtered_content)
+bigram_frequencies = FreqDist(bigrams)
+
+#saves total number of bigrams
+total_bigrams = bigram_frequencies.N()
+#gets the probability of each bigram by dividing its frequency by total bigrams
+bigram_probabilities ={}
+for bigram,frequency in bigram_frequencies.items():
+    bigram_probabilities[bigram] = int(frequency)/total_bigrams
 
 
-stop_words = set(stopwords.words('english'))
-
-words = reuters.words()#tokenizes rueters corpus
-filtered_words =[]
-for word in words:
-    if word not in stop_words:
-        filtered_words.append(word)
-        
-    
-
-#gets a list of all the bigrams and then uses FreqDist to determine the frequencey of each unique bigram and creates a dictionary of this info
-bigrams = bigrams(filtered_words)
-bigram_model = FreqDist(bigrams)
-
-
-for bigram in bigram_model:
-    if bigram[1] == "May":
-        print (bigram_model[bigram])
-        break;
-print(bigram_model.predict("he is"))
-
-'''next_word=''
-highestFreq =0
-for a in bigram_model:
-    print(a["frequency"])
-
-    
-    if bigram[1] == "is" and freq > highestFreq:
-        next_word = bigram[2]
+#question 2
+print("Question 2:")
+next_word = ""
+highest_prob = -1
+#iterates through bigram probabilities searching for bigram that begins with "is" and saves one of highest probability
+for bigram,probability in bigram_probabilities.items():
+    if bigram[0] == "is" and probability > highest_prob:
+        highest_prob = probability
+        next_word = bigram[1]
+#search failed for some reason (has never been true in my testing)
+if (next_word == ""):
+    print ("no next word")
+else:
+    print (f"Next word predicition has produced... 'He is {next_word}'") 
 
 
-        
-print(next_word)
-print(bigram_model.predict("he is"))'''
-        
-    
-    
-
-
-
-
+#question3
+print("Question 3:")   
+sentence = "he is" 
+last_word = "is"
+#repeats previous for loop 5 times to get a full sentence
+for x in range (5):
+    next_word = ""
+    highest_prob = -1
+    #same as before but last_word changes each iteration to reflect the latest word added to the sentence
+    for bigram,probability in bigram_probabilities.items():
+        if bigram[0] == last_word and probability > highest_prob:
+            highest_prob = probability
+            next_word = bigram[1]
+    sentence+= f" {next_word}"#new word added to sentence
+    last_word = next_word#last word changed
+print(sentence+".")
